710d34341e90db200387111bf08341fe4c409f15
MoriHaru568 <karasunosu0627@gmail.com>
2024-01-27 22:49:45 +0900

    blueprintのclassification最適化

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template,redirect, url_for
 
 
 from blueprints.home.views import home
-# from blueprints.analysisMethod.classification import classification
+from blueprints.analysisMethod.classification.views import classification
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "9c1c9ac6-4525-4cf4-8708-f66887ae7641"
 
 app.register_blueprint(home)
-# app.register_blueprint(classification)
+app.register_blueprint(classification)
 
 
 @app.route("/")
--- blueprints/home/views.py
+++ blueprints/home/views.py
@@ -1,13 +1,55 @@
-from flask import  render_template,request,Blueprint,jsonify, session, url_for
-from jinja2 import TemplateNotFound
+from flask import  redirect, render_template,request,Blueprint,url_for
+
+
 
 
 
 FUNCTION_NAME="home"
 home=Blueprint(FUNCTION_NAME,__name__,url_prefix="/"+FUNCTION_NAME,template_folder="templates",static_folder="./static")
 
+
+
 @home.route("/",methods=["GET"])
 def index():
     return render_template(FUNCTION_NAME+"/home.html",message="")
 
+@home.route('/chi-square-test')
+def chi_square_test():
+    return redirect(url_for('classification.chiSquareTest'))
+
+@home.route('/t-test')
+def t_test():
+    return render_template('classification.tTest')
+
+@home.route('/variance')
+def anova():
+    return render_template('classification.variance')
+
+# @home.route('/factor-test')
+# def anova():
+#     return render_template('anova.html')
+
+# @home.route('/principal-component')
+# def anova():
+#     return render_template('anova.html')
+
+# @home.route('/multidimensional-scaling-test')
+# def anova():
+#     return render_template('anova.html')
+
+# @home.route('/correspondence-analysis')
+# def anova():
+#     return render_template('anova.html')
+
+# @home.route('/quantification-test')
+# def anova():
+#     return render_template('anova.html')
+
+# @home.route('/cluster-analysis')
+# def anova():
+#     return render_template('anova.html')
+
+# @home.route('/latent-class-analysis')
+# def anova():
+#     return render_template('anova.html')
 
